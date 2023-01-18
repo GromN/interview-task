95521437e8913e3a56b03b6673a308a8f760be2e
Nataliia Hromenko <nataliia@capitalise.com>
2023-01-18 08:09:50 +0000

    Add a task for the interview, install faker

--- cypress/e2e/test.cy.js/test.cy.js
+++ cypress/e2e/test.cy.js/test.cy.js
@@ -1,5 +1,12 @@
 describe("My First Test", () => {
   it("Sign Up", () => {
-    cy.visit("https://demo.capitalise.com/");
+    cy.visit("https://demo.capitalise.com/app/ui/signup/business");
   });
 });
+
+// Let's create our first e2e test for the Sign Up flow!
+
+// - Create a business user by filling in all the fields of the Sign Up form. 
+// - Validate that you are landing on the 'Today' page at the end of the test.
+
+// You are always welcome to discuss the flow and ask questions!
